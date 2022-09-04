0efbcd0685e862f1c1de1ee66c87c75bedd4e1d7
Investmentbanking <41571606+Investmentbanking@users.noreply.github.com>
2022-09-04 02:05:16 +0100

    adding link to ariana icon

--- js/index.js
+++ js/index.js
@@ -1,5 +1,6 @@
 const navToggle = document.querySelector('.nav-toggle');
 const navLinks = document.querySelectorAll('.nav__link');
+const iconLink = document.querySelector('.intro__img');
 
 navToggle.addEventListener('click', () => {
     document.body.classList.toggle('nav-open');
